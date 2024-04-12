8ef815fd3db382a11c591bc1849868f40294515a
Yoruverse <github@yoruverse.com>
2024-04-12 09:45:37 +0200

    Update navbar-user.blade.php

--- resources/views/components/navbar-user.blade.php
+++ resources/views/components/navbar-user.blade.php
@@ -1,5 +1,5 @@
 <section class="flex items-center gap-5">
-    <p class="text-slate-700">Sesión iniciada como
+    <p class="text-slate-700 sm:block hidden">Sesión iniciada como
         <span class="font-bold">{{ auth()->user()->name }}</span>
     </p>
     <button class="size-9 flex min-w-9 justify-center items-center bg-blue-400 rounded-full">
